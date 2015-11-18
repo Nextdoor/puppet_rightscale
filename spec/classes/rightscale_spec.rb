d114111748f794879c7a58aadd50f657011f27f5
Nathan Valentine <nathan@nextdoor.com>
2015-11-18 11:41:02 -0800

    detect RL10 and modify sudoers accordingly

--- spec/classes/rightscale_spec.rb
+++ spec/classes/rightscale_spec.rb
@@ -10,11 +10,14 @@ FACTS = {
   :lsbdistrelease => '12.04',
   :lsbmajdistrelease => '12',
   :kernel => 'linux',
+  :is_rightscale => true,
 }
 
 describe 'rightscale', :type => 'class' do
-  context 'default params' do
+  context 'default params for RightLink6' do
     let(:facts) { FACTS }
+    let(:facts) { {:rightlink_version => '6.0.0'} }
+    
     it do
       should compile.with_all_deps
       should contain_package('rest-client').with(
@@ -23,6 +26,16 @@ describe 'rightscale', :type => 'class' do
         'ensure' => '3.1.0')
       should contain_package('right_api_client').with(
         'ensure' => '1.5.19')
+    end
+  end
+
+  context 'default params for RightLink10' do
+    let(:facts) { FACTS }
+    let(:facts) { {:rightlink_version => '10.0.0'} }
+    
+    it do
+      should compile.with_all_deps
+      should contain_sudoers__add('includedir for RightLink10')
     end
   end
 end
