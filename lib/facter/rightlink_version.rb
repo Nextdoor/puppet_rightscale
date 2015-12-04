18a573d7e6057405dd1698c26e783a9e9ded09e3
Nathan R Valentine <nrvale0@gmail.com>
2015-12-04 15:42:08 -0800

    new Fact is_rightscale

--- lib/facter/rightlink_version.rb
+++ lib/facter/rightlink_version.rb
@@ -1,58 +1,33 @@
 Facter.add(:rightlink_version) do
   confine :kernel => %w{Linux Windows}
+  confine :is_rightscale => [true, 'true']
   setcode do
 
-    rightlink_facter_debug = ENV.has_key?('RIGHTLINK_FACTER_DEBUG')
-    
     version = nil
     matches = nil
-    
-    # check for RightLink < 10
-    regex = %r{^rs_config (?<version>\d\.\d+\.\d+).*$}
-    matches = regex.match(Facter::Util::Resolution.exec('rs_config --version'))
-    puts "Match status for RightLink < 10: %s" % (matches) if rightlink_facter_debug
+
+    # check for RightLink == 10
+    regex = %r{^\w+ (?<version>\d+\.\d+\.\d+).*$}
+    matches = regex.match(Facter::Util::Resolution.exec('rightlink --version'))
+    Facter.debug("Match status for RightLink 10: %s" % (matches))
     if nil != matches and matches.names.include? 'version'
       version = matches['version']
-      
-    # check for RightLink == 10
     else
-      regex = %r{^\w+ (?<version>\d+\.\d+\.\d+).*$}
-      matches = regex.match(Facter::Util::Resolution.exec('rightlink --version'))
-      puts "Match status for RightLink 10: %s" % (matches) if rightlink_facter_debug
+      
+      # check for RightLink < 10
+      regex = %r{^rs_config (?<version>\d\.\d+\.\d+).*$}
+      matches = regex.match(Facter::Util::Resolution.exec('rs_config --version'))
+      Facter.debug("Match status for RightLink < 10: %s" % (matches))
       if nil != matches and matches.names.include? 'version'
         version = matches['version']
       end
     end
 
-    if nil != version
-      version.to_s
-    else
-      version
+    unless nil == version
+      version = version.to_s
     end
     
-  end
-end
-
-Facter.add(:rightlink_maj_version) do
-  confine :kernel => %w{Linux Windows}
-  setcode do
-
-    rightlink_facter_debug = ENV.has_key?('RIGHTLINK_FACTER_DEBUG')
-
-    rightlink_version = Facter.value(:rightlink_version)
-    puts "rightlink_version: %s" % (rightlink_version) if rightlink_facter_debug
-    
-    majversion = nil
-
-    if rightlink_version
-      majversion = rightlink_version.split('.')[0]
-    end
-
-    if nil != majversion
-      majversion.to_i
-    else
-      majversion
-    end
+    version
     
   end
 end
--- spec/unit/facter/rightlink_version_spec.rb
+++ spec/unit/facter/rightlink_version_spec.rb
@@ -1,69 +1,48 @@
 require 'spec_helper'
+require 'facter/is_rightscale'
 require 'facter/rightlink_version'
 
 describe Facter::Util::Fact do
-  
-  before do
-    Facter.clear
-    Facter.clear_messages
-    allow(Facter.fact(:kernel)).to receive(:value).and_return("Linux")
-  end
-
-  after do
-    Facter.clear
-    Facter.clear_messages
-  end
 
   #
   # RightLink version testing
   #
   describe "rightlink_version" do
+
+    before do
+      Facter.clear
+      Facter.clear_messages
+      allow(Facter.fact(:kernel)).to receive(:value).and_return("Linux")
+      allow(Facter.fact(:is_rightscale)).to receive(:value).and_return(true)
+      allow(Facter::Util::Resolution).to receive(:exec).with('rs_config --version').and_return(nil)
+      allow(Facter::Util::Resolution).to receive(:exec).with('rightlink --version').and_return(nil)
+    end
     
-    context "RightLink is not installed" do
-      it "should return the nil value" do
-        expect(Facter.fact(:rightlink_version).value).to eq(nil)
-      end
+    after do
+      Facter.clear
+      Facter.clear_messages
     end
     
     context "RightLink6 is installed" do
-      before do
-        allow(Facter::Util::Resolution).to receive(:exec).with('rs_config --version').and_return("rs_config 6.0.0")
-      end
-      
       it "should return String '6.0.0'" do
+        allow(Facter::Util::Resolution).to receive(:exec).with('rs_config --version').and_return("rs_config 6.0.0")
         expect(Facter.fact(:rightlink_version).value).to eq('6.0.0')
       end
     end
     
     context "RightLink10 is installed" do
-      before do
-        allow(Facter::Util::Resolution).to receive(:exec).with('rs_config --version').and_return(nil)
-        allow(Facter::Util::Resolution).to receive(:exec).with('rightlink --version').and_return("rightlink 10.0.0")
-      end
-      
       it "should return String '10.0.0'" do
+        allow(Facter::Util::Resolution).to receive(:exec).with('rightlink --version').and_return("rightlink 10.0.0")
         expect(Facter.fact(:rightlink_version).value).to eq('10.0.0')
       end
     end
-  end
-
-  #
-  # RightLink major version Fact testing
-  #
-  describe "rightlink_maj_version" do
-
-    context "Fact 'rightlink_version' does not exist" do
-      it "should return value nil" do
-        allow(Facter.fact(:rightlink_version)).to receive(:value).and_return(nil)
-        expect(Facter.fact(:rightlink_maj_version).value).to eq(nil)
+    
+    context "RightLink is not installed" do
+      it "should return the nil value" do
+        allow(Facter.fact(:is_rightscale)).to receive(:value).and_return(false)
+        expect(Facter.fact(:rightlink_version).value).to eq(nil)
       end
     end
-  
-    context "Fact 'rightlink_version' exists" do
-      it "should return the Integer 10 if 'rightlink_version' is '10.0.0'" do
-        allow(Facter.fact(:rightlink_version)).to receive(:value).and_return('10.0.0')
-        expect(Facter.fact(:rightlink_maj_version).value).to eq(10)
-      end
-    end
+    
   end
 end
