d37413431d2274ceccaebcec06b378ce3dbbb510
Matt Wise <matt@nextdoor.com>
2016-01-05 12:46:30 -0800

    Use the ruby-rest-client package on Ubuntu.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,5 +11,24 @@ class rightscale::params {
   # statement. For now, this supports Ubuntu 10/12 with Ruby 1.8.
   $right_api_client = '1.5.19'
   $right_aws        = '3.1.0'
-  $rest_client      = '1.6.7'
+
+  # The rest-client gem/package is tricky. On older Ubuntu hosts, its safer to
+  # just install the ruby-rest-client debian package, and then install the
+  # right_api_client with "--ignore-dependencies". On newer Ubuntu hosts, the
+  # Gem works fine .. but its not perfect.
+  #
+  # To handle this more sanely, on non-Ubunth hosts, we'll ue the Gem for now.
+  # On Ubuntu hosts, we'll use the ruby-rest-client package.
+  case $::operatingsystem {
+    'Ubuntu': {
+      $rest_provider = 'apt'
+      $rest_package  = 'ruby-rest-client'
+      $rest_client   = 'installed'
+    }
+    default: {
+      $rest_provider = 'gem'
+      $rest_package  = 'rest-client'
+      $rest_client   = '1.6.7'
+    }
+  }
 }
--- spec/classes/rightscale_spec.rb
+++ spec/classes/rightscale_spec.rb
@@ -4,14 +4,14 @@ describe '::rightscale', :type => 'class' do
 
   let! :facts do
     {
-      :osfamily => 'Debian',
-      :operatingsystem => 'Ubuntu',
+      :osfamily               => 'Debian',
+      :operatingsystem        => 'Ubuntu',
       :operatingsystemrelease => '12',
-      :lsbdistid => 'Ubuntu',
-      :lsbdistcodename => 'precise',
-      :lsbdistrelease => '12.04',
-      :lsbmajdistrelease => '12',
-      :kernel => 'linux',
+      :lsbdistid              => 'Ubuntu',
+      :lsbdistcodename        => 'precise',
+      :lsbdistrelease         => '12.04',
+      :lsbmajdistrelease      => '12',
+      :kernel                 => 'linux',
     }
   end
   
@@ -20,15 +20,16 @@ describe '::rightscale', :type => 'class' do
     it do
       facts.merge!(
         {
-          :is_rightscale => true,
-          :rightlink_version => '6.0.0',
+          :is_rightscale         => true,
+          :rightlink_version     => '6.0.0',
           :rightlink_maj_version => 6
         }
       )
       
       should compile.with_all_deps
-      should contain_package('rest-client').with(
-        'ensure' => '1.6.7')
+      should contain_package('ruby-rest-client').with(
+        'ensure'   => 'installed',
+        'provider' => 'apt')
       should contain_package('right_aws').with(
         'ensure' => '3.1.0')
       should contain_package('right_api_client').with(
@@ -36,12 +37,34 @@ describe '::rightscale', :type => 'class' do
     end
   end
 
+  context 'CentOS and RightLink' do
+
+    it do
+      facts.merge!(
+        {
+          :operatingsystem       => 'CentOS',
+          :is_rightscale         => true,
+          :rightlink_version     => '6.0.0',
+          :rightlink_maj_version => 6
+        }
+      )
+      
+      should compile.with_all_deps
+      should contain_package('rest-client').with(
+        'ensure'   => '1.6.7',
+        'provider' => 'gem')
+      should contain_package('right_aws').with(
+        'ensure' => '3.1.0')
+      should contain_package('right_api_client').with(
+        'ensure' => '1.5.19')
+    end
+  end
   context 'default params and RightLink10' do
     it do
       facts.merge!(
         {
-          :is_rightscale => true,
-          :rightlink_version => '10.0.0',
+          :is_rightscale         => true,
+          :rightlink_version     => '10.0.0',
           :rightlink_maj_version => 10
         }
       )
