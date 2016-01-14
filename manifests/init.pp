e637c4f72d2fd480ad3c49224afdab98383beecd
Nathan R Valentine <nrvale0@gmail.com>
2016-01-13 17:03:53 -0800

    add more sophisticted sudo rule handling

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,16 +13,22 @@
 # [*rest_client*]
 #   Supporting rest_client Gem Version to Install
 #
+# [*manage_rightlink_sudo*]
+#   Drop a sudoers file for necessary RightLink perms.
+#   If set to Boolean false sudoers rules will be left as
+#   populated by RightLink installer.
+#
 # === Authors
 #
 # Matt Wise <matt@nextdoor.com>
 #
 class rightscale (
-  $right_api_client = $rightscale::params::right_api_client,
-  $right_aws        = $rightscale::params::right_aws,
-  $rest_client      = $rightscale::params::rest_client,
-  $rest_package     = $rightscale::params::rest_package,
-  $rest_provider    = $rightscale::params::rest_provider,
+  $right_api_client       = $rightscale::params::right_api_client,
+  $right_aws              = $rightscale::params::right_aws,
+  $rest_client            = $rightscale::params::rest_client,
+  $rest_package           = $rightscale::params::rest_package,
+  $rest_provider          = $rightscale::params::rest_provider,
+  $manage_rightlink_sudo  = true
   ) inherits ::rightscale::params {
 
   # pretty sure our version of stdlib does not include validate_integer
@@ -31,11 +37,40 @@ class rightscale (
   unless is_integer($::rightlink_maj_version) {
     fail("\$::rightlink_maj_version is \'${::rightlink_maj_version}\' but must be Integer!")
   }
-  
+
+  validate_bool($manage_rightlink_sudo)
+
+  if $manage_rightlink_sudo { include ::sudo }
+
   case $::rightlink_maj_version {
-    10: { include ::sudo }
-    
+    10: {
+      if $manage_rightlink_sudo {
+        sudo::conf { 'rightlink10_sudoers':
+          content => template(
+            "${module_name}/header.erb",
+            "${module_name}/rightlink10_sudo.erb"
+          )
+        }
+      }
+    }
+
     6: {
+      if $manage_rightlink_sudo {
+        sudo::conf { 'rightscale_users':
+          content => template(
+            "${module_name}/header.erb",
+            "${module_name}/rightscale_users_sudo.erb"
+          )
+        }
+
+        sudo::conf { 'rightscale':
+          content => template(
+            "${module_name}/header.erb",
+            "${module_name}/rightscale_sudo.erb"
+            )
+        }
+      }
+
       # Install the required gems in order. These gems support the custom
       # Hiera backend as well as the puppet rs-facts plugin that gathers
       # common RightScale facts.
@@ -44,12 +79,12 @@ class rightscale (
         $rest_package:
           ensure   => $rest_client,
           provider => $rest_provider;
-        
+
         'right_aws':
           ensure   => $right_aws,
           provider => 'gem',
           require  => Package[$rest_package];
-        
+
         'right_api_client':
           ensure          => $right_api_client,
           provider        => 'gem',
@@ -57,7 +92,7 @@ class rightscale (
           require         => Package[$rest_package];
       }
     }
-    
+
     default: {
       fail("RightLink ${::rightlink_maj_version} is not supported by this module!")
     }
--- spec/classes/rightscale_spec.rb
+++ spec/classes/rightscale_spec.rb
@@ -12,33 +12,82 @@ describe '::rightscale', :type => 'class' do
       :lsbdistrelease         => '12.04',
       :lsbmajdistrelease      => '12',
       :kernel                 => 'linux',
+      :is_rightscale          => true,
     }
   end
-  
-  context 'default params and RightLink6' do
 
+  context 'default params and RightLink6' do
     it do
       facts.merge!(
         {
-          :is_rightscale         => true,
           :rightlink_version     => '6.0.0',
           :rightlink_maj_version => 6
         }
       )
-      
+
       should compile.with_all_deps
-      should contain_package('ruby-rest-client').with(
-        'ensure'   => 'installed',
-        'provider' => 'apt')
+
       should contain_package('right_aws').with(
-        'ensure' => '3.1.0')
+               'ensure' => '3.1.0')
       should contain_package('right_api_client').with(
-        'ensure' => '1.5.19')
+               'ensure' => '1.5.19')
+
+      should contain_class('sudo')
+      should contain_sudo__conf('rightscale_users').with(
+               'content' => /%rightscale_sudo\s+ALL=NOPASSWD\:\s+ALL/)
+      should contain_sudo__conf('rightscale').with(
+               'content' => /rightscale\s+ALL=\(ALL\)NOPASSWD/)
+    end
+  end
+
+  context 'default params and RightLink10' do
+    it do
+      facts.merge!(
+        {
+          :rightlink_version     => '10.0.0',
+          :rightlink_maj_version => 10
+        }
+      )
+
+      should compile.with_all_deps
+      should contain_class('sudo')
+      should contain_sudo__conf('rightlink10_sudoers').with(
+               'content' => /rightlink\s+ALL=\(ALL\)\s+SETENV:NOPASSWD\:ALL/)
+    end
+  end
+
+  context 'RightLink6 manage_rightlink_sudo == false' do
+    let(:params) { { :manage_rightlink_sudo => false } }
+    it do
+      facts.merge!(
+        {
+          :rightlink_version => '6.0.0',
+          :rightlink_maj_version => 6
+        }
+      )
+
+      should_not contain_class('sudo')
+      should_not contain_sudo__conf('rightlink10_sudoers')
+    end
+  end
+
+  context 'RightLink10 manage_rightlink_sudo == false' do
+    let(:params) { { :manage_rightlink_sudo => false } }
+    it do
+      facts.merge!(
+        {
+          :rightlink_version => '10.0.0',
+          :rightlink_maj_version => 10
+        }
+      )
+
+      should_not contain_class('sudo')
+      should_not contain_sudo__conf('rightscale_users')
+      should_not contain_sudo__conf('rightscale')
     end
   end
 
   context 'CentOS and RightLink' do
-
     it do
       facts.merge!(
         {
@@ -48,29 +97,12 @@ describe '::rightscale', :type => 'class' do
           :rightlink_maj_version => 6
         }
       )
-      
+
       should compile.with_all_deps
-      should contain_package('rest-client').with(
-        'ensure'   => '1.6.7',
-        'provider' => 'gem')
       should contain_package('right_aws').with(
         'ensure' => '3.1.0')
       should contain_package('right_api_client').with(
         'ensure' => '1.5.19')
-    end
-  end
-  context 'default params and RightLink10' do
-    it do
-      facts.merge!(
-        {
-          :is_rightscale         => true,
-          :rightlink_version     => '10.0.0',
-          :rightlink_maj_version => 10
-        }
-      )
-      
-      should compile.with_all_deps
-      should contain_class('sudo')
     end
   end
 
