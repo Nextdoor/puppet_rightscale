a6122db5f9202c59f68d5798ad44a6f0e5a15c03
Nathan R Valentine <nrvale0@gmail.com>
2016-02-04 13:51:26 -0800

    ubuntu12.04 gets right_api_client 1.5.26 and no later

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,28 +7,28 @@
 # Matt Wise <matt@nextdoor.com>
 #
 class rightscale::params {
-  # TODO: When supporting other OS's, will update this class with a big case
-  # statement. For now, this supports Ubuntu 10/12 with Ruby 1.8.
-  $right_api_client = '1.5.19'
-  $right_aws        = '3.1.0'
 
   # The rest-client gem/package is tricky. On older Ubuntu hosts, its safer to
   # just install the ruby-rest-client debian package, and then install the
   # right_api_client with "--ignore-dependencies". On newer Ubuntu hosts, the
   # Gem works fine .. but its not perfect.
-  #
-  # To handle this more sanely, on non-Ubunth hosts, we'll ue the Gem for now.
-  # On Ubuntu hosts, we'll use the ruby-rest-client package.
-  case $::operatingsystem {
-    'Ubuntu': {
-      $rest_provider = 'apt'
-      $rest_package  = 'ruby-rest-client'
-    }
-    default: {
-      $rest_provider = 'gem'
-      $rest_package  = 'rest-client'
-    }
-  }
 
-  $rest_client = 'installed'
+  # Ubuntu 12.04 requires right_api_client <= 1.5.26 due to deps on
+  # 'base64' module shipped with Ruby 1.8.7.
+
+  if 'Ubuntu' == $::operatingsystem {
+    $rest_provider = 'apt'
+    $rest_package  = 'ruby-rest-client'
+
+    if '12.04' == $::operatingsystemmajrelease {
+      $right_api_client = '1.5.26'
+    }
+    
+  } else {
+    $rest_provider = 'gem'
+    $rest_package = 'rest-client'  
+    $right_api_client = 'installed'
+  }
+  
+  $right_aws        = '3.1.0'
 }
